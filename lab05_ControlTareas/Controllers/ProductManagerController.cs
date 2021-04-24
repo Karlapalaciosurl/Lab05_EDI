89909e5c4487a03a2fadd4f2a8b2efa6016e07e2
karlapalaciosurl <Karli.508palacios@hotmail.com>
2021-04-23 22:41:19 -0600

    realizacion de la vista y controladores para la agenda por usuario en donde se pueden ver las tasks y dar como realizada la task con mayor prioridad, y area de dashboard para product manager en donde se pueden ver las tareas del equipo de trabajo

---
+++ lab05_ControlTareas/Controllers/ProductManagerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab05_ControlTareas.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
